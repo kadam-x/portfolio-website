c2459f69b879d704b23088d243ad4adc50abac35
kadam-x <sn33zyhub@gmail.com>
2026-05-08 13:22:41 +0200

    fixed broken script

--- scripts/generate-git-log.mjs
+++ scripts/generate-git-log.mjs
@@ -1,6 +1,8 @@
 import { execSync } from "child_process";
 import { writeFileSync } from "fs";
 
+mkdirSync("src/data", { recursive: true });
+
 const output = execSync(
   "git log --date=format:%Y-%m-%d --format='%ad %s' -45",
   { encoding: "utf-8" },
