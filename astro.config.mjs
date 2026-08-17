10f840bcbc7578c4c82d48b743d912ba6e6a55cd
kadam-x <sn33zyhub@gmail.com>
2026-08-17 18:06:11 +0200

    refactor(everything): new simpler style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "hover",
